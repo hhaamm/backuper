dc29e1bfdbc1330ab779b7df05ebe6ca2b1c2556
hhaamm <ham1988@gmail.com>
2012-05-13 20:30:41 -0300

    Little fix

--- lib/mysql_backup.rb
+++ lib/mysql_backup.rb
@@ -10,7 +10,6 @@ class MySQLBackup
 	def run
 		destiny_file = File.join(@tmp_folder, @name+'.sql')
 		command = "mysqldump -u #{@username} --password=#{@password} #{@db} > #{destiny_file}"
-		puts command
 		if !system(command)
 			puts "An error has ocurred while backuping MySQL database '#{@db}'"
 		end
