9558e4c06ce3481256fae01f37816513d4533aed
hhaamm <ham1988@gmail.com>
2012-05-19 14:10:14 -0300

    Added file rotations

--- lib/backuper_remote.rb
+++ lib/backuper_remote.rb
@@ -4,7 +4,7 @@ class BackuperRemote
 		@tmp_folder = config[:tmp_folder]
 		@rotations = config[:rotations]
 		@destiny_path = config[:destiny_path]
-		@date_format = '%Y_%m_%d'
+		@date_format = config[:date_format] ? config[:date_format] : '%Y_%m_%d'
 	end
 
 	def run
@@ -21,11 +21,26 @@ class BackuperRemote
 		end
 
 		filename = File.join(@destiny_path, Time.now.strftime(@date_format)+'.tar.gz')
+		puts "Filename: #{filename}"
 		system("tar -cf #{filename} #{@tmp_folder}")
+
+		rotate_files
 	end
 
 	def check_config
 
+	end
+
+	def rotate_files
+		if !@rotations
+			return
+		end
+
+		files = Dir[File.join(@destiny_path, '**', '*')].sort
+
+		for i in (@rotations..(files.length-1)) do
+			File.delete(files[i])
+		end
 	end
 
 	def check_folders
--- test/backuper_remote_spec.rb
+++ test/backuper_remote_spec.rb
@@ -1,25 +1,49 @@
 require '../lib/include.rb'
-require 'config.rb'
+require './config.rb'
+
+include TestConfig
+
+def clean
+	# deleting if directories exists
+	if File.directory? TMP_FOLDER
+		puts "Deleting #{TMP_FOLDER}"
+		FileUtils.rm_rf TMP_FOLDER
+	end
+
+	if File.directory? DESTINY_PATH
+		puts "Deleting #{DESTINY_PATH}"
+		FileUtils.rm_rf DESTINY_PATH
+	end
+end
 
 describe BackuperRemote do
 	describe "#check_folders" do
 		it "create folders if folders doesn't exists yet" do
-			# deleting if directories exists
-			if File.directory? test_tmp_folder
-				puts "Deleting #{test_tmp_folder}"
-				FileUtils.rm_rf test_tmp_folder
-			end
+			clean
 
-			if File.directory? test_destiny_path
-				puts "Deleting #{test_destiny_path}"
-				FileUtils.rm_rf test_destiny_path
-			end
-
-			backuper = BackuperRemote.new(:tmp_folder => test_tmp_folder, :destiny_path => test_destiny_path)
+			backuper = BackuperRemote.new(:tmp_folder => TMP_FOLDER, :destiny_path => DESTINY_PATH)
 			backuper.check_folders
 
-			File.directory?(test_tmp_folder).should == true
-			File.directory?(test_destiny_path).should == true
+			File.directory?(TMP_FOLDER).should == true
+			File.directory?(DESTINY_PATH).should == true
+		end
+	end
+
+	describe '#rotate_files' do
+		it "deletes old backup files when there are more backups than the specified in rotations config option" do
+			clean
+
+			backuper = BackuperRemote.new(:tmp_folder => TMP_FOLDER, :destiny_path => DESTINY_PATH, :rotations => 2, :date_format => "%m_%e_%y_%H_%M_%S", :backups => [FolderBackup.new(:path => './files', :name => 'test_folder')])
+
+			Dir.mkdir DESTINY_PATH
+
+			# we copy 3 arbitrary files to the dir...
+			FileUtils.cp_r './files/.', DESTINY_PATH
+			backuper.rotate_files
+
+			# we specified only 2 rotations, that means that there should be only two files in the folder
+			file_count = Dir[File.join(DESTINY_PATH, '**', '*')].count { |file| File.file?(file) }
+			file_count.should == 2
 		end
 	end
 
