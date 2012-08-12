ce3d20c35745c14525d2c33a2928c2928665019c
hhaamm <ham1988@gmail.com>
2012-08-12 12:49:12 -0300

    Deleted .rb config files

--- samples/mail_backup_config.rb
+++
@@ -1,8 +0,0 @@
-module Config
-  CONFIG = {
-    :backups => [
-                 FolderBackup.new(:path => '/home/myuser/somefolder', :name => 'mailed_backup')],
-    :mail_target => 'myemailaddress@gmail.com',
-    :attachment_extension => '.tar.gz.txt'
-  }
-end
